9991f9834f7ee291e3a4e1b3fc9c1f54f97ffa8b
Kirill Kiselev <k.kiselev@s7.ru>
2025-03-13 00:54:58 +0300

    add README.md

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-scp ../indicator-extractor/hello_world.py ladimirovich@192.168.31.38:/home/ladimirovich/indicator-extractor/
-
-ssh ladimirovich@192.168.31.38
-
-python /home/ladimirovich/indicator-extractor/hello_world.py
